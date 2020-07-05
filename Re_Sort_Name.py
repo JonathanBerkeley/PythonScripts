1c266883854c841a1229ae99633dc5aaeee730f2
JonathanBerkeley <35268549+JonathanBerkeley@users.noreply.github.com>
2020-07-05 20:25:21 +0100

    Add files via upload
    For flipping first names with last names in a file of names separated with newlines
    firstname, lastname -> lastname, firstname

---
+++ Re_Sort_Name.py
@@ -0,0 +1,21 @@
+#Setup
+import sys
+sys.stdout = open('output.txt', "w")
+inc = 0
+
+#Resort lines
+with open("sortme.txt", "r+") as file:
+    for line in file:
+        inc += 1
+        line = line.split(" ")
+        line[len(line)-1] = line[len(line)-1].replace('\n', '')
+        line.append(line.pop(line.index(line[0].rstrip())))
+        line[len(line)-1] += '\n'
+        if (len(line) == 1):
+            print(line[0], end = '')
+        elif (len(line) == 2):
+            print(line[0] + " " +  line[1], end = '')
+        elif (len(line) == 3):
+            print(line[0] + " " +  line[1] + " " + line[2], end = '')
+        elif (len(line) == 4):
+            print(line[0] + " " +  line[1] + " " + line[2] + " " + line[3], end = '')
