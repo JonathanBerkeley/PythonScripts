a376fe996e9118f2b30332bcf17c536f51945867
JonathanBerkeley <35268549+JonathanBerkeley@users.noreply.github.com>
2020-07-20 02:25:40 +0100

    Minor improvements

--- golaunch.py
+++ golaunch.py
@@ -1,12 +1,21 @@
 from subprocess import call
 import sys
+
+def callfile(path):
+    try:
+        call(path, 1000)
+    except(FileNotFoundError):
+        print(path + " was not found, make sure to include directory path")
+
 """Calls files/exes supplied as arguments given in console, and displays any stdout information to command line."""
 if __name__ == "__main__":
-    for args in enumerate(sys.argv):
-        if args[0] > 0:
-            try:
-                call(str(args[1]), 1000)
-            except(FileNotFoundError):
-                print(str(args[1]) + " was not found, make sure to include directory path")
+    if len(sys.argv) < 2:
+        try:
+            callfile(str(input('Enter the file\'s filepath to execute:\n ')))
+        except:
+            print("Bad input")
+    else:
+        callfile(sys.argv[1])
+        
 else:
     raise SystemExit
